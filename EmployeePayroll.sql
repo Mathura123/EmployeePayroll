48c5e22d0d093a5601099a3e6c6400353aeb6266
Mathura <mathuradas333@gmail.com>
2020-11-03 08:43:14 +0530

    [Mathura]Add. Added new column named 'gender' in employee_payroll and set the values of gender for Bill, Terissa and Charlie

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -7,6 +7,7 @@ SELECT name FROM sys.databases;
 use payroll_service;
 --gives the name of database in use (now payroll_service)
 select DB_NAME();
+
 --creates table named employee_payroll in payroll_service database
 create table employee_payroll
 (
@@ -17,14 +18,28 @@ start date not null
 );
 --gives the info about the table named employee_payroll
 select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'employee_payroll';
+
 --Insert datas in table employee_payroll
 insert into employee_payroll values
 ('Bill',100000.00,'2018-01-03'),
 ('Terissa',200000.00,'2019-11-13'),
 ('Charlie',300000.00,'2020-05-21');
+
 --retrives all datas in employee_payroll
 select * from employee_payroll;
+
 --to retrieve data of Bill's salary
 select salary from employee_payroll where name = 'Bill';
 --to retrieve datas of employees in between 2018-01-01 and today
 select * from employee_payroll where start between '2018-01-01' and GETDATE();
+
+--adds new column named 'gender'
+alter table employee_payroll
+add gender char;
+--gives the info about the table named employee_payroll
+select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'employee_payroll';
+--set values of gender for Bill, Terissa and Charlie
+update employee_payroll set gender = 'M' where name = 'Bill' or name = 'Charlie';
+update employee_payroll set gender = 'F' where name = 'Terissa';
+--retrives all datas in employee_payroll
+select * from employee_payroll;
