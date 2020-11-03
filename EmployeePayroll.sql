99b2f3c6f507026e39e1fa93f3d0dab8ff751b86
Mathura <mathuradas333@gmail.com>
2020-11-03 08:24:04 +0530

    [Mathura]Add. Ability to retrive all datas in employee_payroll table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -22,5 +22,5 @@ insert into employee_payroll values
 ('Bill',100000.00,'2018-01-03'),
 ('Terissa',200000.00,'2019-11-13'),
 ('Charlie',300000.00,'2020-05-21');
---views all datas in employee_payroll
+--retrives all datas in employee_payroll
 select * from employee_payroll;
