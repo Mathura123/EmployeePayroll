951da55deffd4a9dbcd5df75fca198be590bd4e7
Mathura <mathuradas333@gmail.com>
2020-11-06 09:07:43 +0530

    [Mathura]Add. Implement the ER diagram into payroll service DB

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -74,3 +74,85 @@ alter table employee_payroll add tax float;
 insert into employee_payroll 
 values('Terissa',200000,'2018-10-13','F',null,'Marketing','Mumbai',12000,1880,120000,12000);
 select * from employee_payroll where name = 'Terissa';
+
+--created table company
+create table company
+(
+company_id int identity(1,1) not null primary key,
+company_name varchar(150) not null
+);
+--created table employee
+create table employee
+(
+employee_id int identity(1,1) primary key,
+company_id int not null foreign key references company(company_id),
+name varchar(150) not null,
+gender varchar(1),
+phone_no varchar(15),
+address varchar(150) not null default('India')
+);
+--created table department
+create table department
+(
+dept_id int identity(100,1) primary key,
+dept_name varchar(50) not null,
+);
+--created table employee_department for many to many relationship
+create table employee_department
+(
+employee_id int not null foreign key references employee(employee_id),
+dept_id int not null foreign key references department(dept_id) 
+);
+--created table payroll. Here taxable_pay and net_pay are derived attributes
+create table payroll
+(
+employee_id int not null foreign key references employee(employee_id),
+Start date not null,
+basic_pay money not null,
+deduction money,
+taxable_pay as basic_pay-deduction,
+income_tax money,
+net_pay as basic_pay-(income_tax+deduction)
+);
+--described all tables
+exec sp_columns company;
+exec sp_columns employee;
+exec sp_columns department; 
+exec sp_columns employee_department;
+exec sp_columns payroll; 
+--inserted values in company table
+insert into company values
+('Company1'),
+('Company2'),
+('Company3');
+--inserted values in employee table
+insert into employee values
+(1,'Bill','M','9999999999','Nagar colony'),
+(1,'Terissa', 'F', '8888888888',default),
+(1,'Charlie', 'M', '8888888888', 'Dalal Street'),
+(2,'Clinton','M','4545454545','Andheri road');
+--inserted values in department table
+insert into department values
+('Marketing'),
+('Sales'),
+('Engineering'),
+('HR');
+--inserted values in employee_department table
+insert into employee_department values
+(1,100),
+(2,100),
+(2,101),
+(3,103),
+(4,102);
+--inserted values in payroll table. Only employee_id,start_date,basic_pay,deductions,income_tax inserted. Others are derived
+insert into payroll values
+(1,'01-20-1998',200000,12000,10000),
+(2,'12-06-1995',300000,15000,20000),
+(3,'11-02-2020',100000,5000,2000),
+(4,'10-01-2020',30000,2000,0);
+--for viewing inserted values
+select * from company;
+select * from employee;
+select * from department;
+select * from employee_department;
+select * from payroll;
